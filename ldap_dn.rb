d2ce84598ff8b19a4f5b4a5d6240ce59e91a2ede
root <root@tma-pup.tmalab.local>
2014-07-17 14:41:20 -0400

    ldap_dn facter

--- ldap_dn.rb
+++ ldap_dn.rb
@@ -1,5 +1,5 @@
 Facter.add('ldap_dn') do
 	setcode do
-    	Facter::Util::Resolution.exec('/bin/dnsdomainname | sed -e \'s/^/dc=/\' -e \'s/\./,dc=/\'')
+		Facter::Util::Resolution.exec('/bin/dnsdomainname | sed -e \'s/^/dc=/\' -e \'s/\./,dc=/\'')
 	end
 end
